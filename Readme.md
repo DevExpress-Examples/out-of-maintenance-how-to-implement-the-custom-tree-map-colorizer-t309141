8138710b23d4fbc4daf543c4b1317d922262ea46
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to implement the custom Tree Map Colorizer
+
+
+This example demonstrates how to implement the custom Tree Map colorizer.
+
+
+<h3>Description</h3>
+
+To implement the custom colorizer, inherit the&nbsp;<a href="https://documentation.devexpress.com/#WPF/clsDevExpressXpfTreeMapTreeMapColorizerBasetopic">TreeMapColorizerBase</a>&nbsp;or its descendant class, and override the&nbsp;<a href="https://documentation.devexpress.com/#WPF/DevExpressXpfTreeMapTreeMapColorizerBase_GetItemColortopic">GetItemColor</a><strong>&nbsp;</strong>method.&nbsp;<br>Sent as parameters,&nbsp;<a href="https://documentation.devexpress.com/#WPF/clsDevExpressXpfTreeMapTreeMapItemtopic">TreeMapItem</a>&nbsp;and&nbsp;<a href="https://documentation.devexpress.com/#WPF/clsDevExpressXpfTreeMapTreeMapItemGroupInfotopic">TreeMapItemGroupInfo</a>&nbsp;objects contain all the required information to colorize items.
+
+<br/>
+
+
---
+++ VB/CustomColorizerSample/CustomColorizer.vb
@@ -0,0 +1,25 @@
+﻿Imports DevExpress.Xpf.TreeMap
+Imports System.Windows.Media
+
+
+Namespace CustomColorizerSample
+    Friend Class CustomColorizer
+        Inherits TreeMapPaletteColorizerBase
+
+        Public Overrides Function GetItemColor(ByVal item As TreeMapItem, ByVal group As TreeMapItemGroupInfo) As Color?
+            If item.Children.Count = 0 Then
+                Dim itemColor As Color = Palette(group.ItemIndex Mod Palette.Count)
+                Dim groupColor As Color = Palette(group.GroupIndex Mod Palette.Count)
+                Dim proportion As Double = (item.Value - group.MinValue) / (group.MaxValue - group.MinValue)
+
+                Return New Color With {.A = 255, .R = CByte(proportion * itemColor.R + (1-proportion)*groupColor.R), .G = CByte(proportion * itemColor.G + (1 - proportion) * groupColor.G), .B = CByte(proportion * itemColor.B + (1 - proportion) * groupColor.B)}
+            Else
+                Return Palette(Palette.Count - 1 - group.GroupLevel Mod Palette.Count)
+            End If
+        End Function
+
+        Protected Overrides Function CreateObject() As TreeMapDependencyObject
+            Return New CustomColorizer()
+        End Function
+    End Class
+End Namespace
